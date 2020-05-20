48bbae077d4fadb44d471d98a5f1f3fe6a4767f0
owen0 <owen0@LAPTOP-7VH12NO6>
2020-05-20 11:24:08 +0800

    merge Code

--- CMAP/src/com/cmap/Env.java
+++ CMAP/src/com/cmap/Env.java
@@ -131,6 +131,7 @@ public class Env {
 	public static String MAIL_SERVER_PASSWORD;
 
 	public static Integer SEND_COMMAND_SLEEP_TIME;                           // 設定發送多條命令的間格時間(毫秒)
+	public static String ENABLE_GET_IP_FROM_INFO;                      // 設定啟用或停用查詢未納管IP來源資訊驗證(Y/N)
 	public static String GET_IP_FROM_INFO_API_URL;                           // 設定查詢IP來源資訊的網站API URL (http://ip-api.com/docs/)
 	public static String GET_IP_FROM_INFO_WEB_SITE_URL;                      // 設定查詢IP來源資訊的網站URL (https://dnslytics.com/，由苗栗教網老師提供)
 
@@ -283,6 +284,7 @@ public class Env {
 
 	public static List<String> TELNET_LOGIN_USERNAME_TEXT  = new ArrayList<>();
 	public static String TELNET_LOGIN_PASSWORD_TEXT;
+	public static List<String> TELNET_LOGIN_ENABLE_TEXT  = new ArrayList<>();
 	public static Integer TELNET_READ_UNTIL_MAX_RUNTIME;
 
 	public static Integer SSH_CONNECT_TIME_OUT;
