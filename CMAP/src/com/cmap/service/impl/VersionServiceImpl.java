971ab7e34c3ee6b3ec0246643966497fe0ba3008
owen0 <owen0@LAPTOP-7VH12NO6>
2020-10-13 15:21:44 +0800

    cmm

--- CMAP/src/com/cmap/service/impl/VersionServiceImpl.java
+++ CMAP/src/com/cmap/service/impl/VersionServiceImpl.java
@@ -694,7 +694,7 @@ public class VersionServiceImpl extends CommonServiceImpl implements VersionServ
 								fileDir = date_yyyyMMdd.concat(Env.FTP_DIR_SEPARATE_SYMBOL).concat(fileDir);
 							}
 						}
-						log.debug("for debug fileDir = " + fileDir);
+						log.info("for bedug fileDir = " + fileDir);
 						fileUtils.changeDir(fileDir, false);
 
 						// Step4. 下載指定的Config落地檔
@@ -857,6 +857,88 @@ public class VersionServiceImpl extends CommonServiceImpl implements VersionServ
 			masterVO.setBeginTime(new Date());
 			masterVO.setUserName(jobTrigger ? Env.USER_NAME_JOB : (StringUtils.isBlank(triggerName)?SecurityUtil.getSecurityUser().getUsername():triggerName));
 
+			StepServiceVO ssVO;
+			for (String deviceListId : deviceListIDs) {
+				ssVO = stepService.doBackupStep(deviceListId, jobTrigger);
+
+				masterVO.getDetailVO().addAll(ssVO.getPsVO().getDetailVO());
+
+				successCount += ssVO.isSuccess() && (ssVO.getResult() != Result.NO_DIFFERENT) ? 1 : 0;
+				errorCount += !ssVO.isSuccess() ? 1 : 0;
+				noDiffCount += ssVO.getResult() == Result.NO_DIFFERENT ? 1 : 0;
+
+				//log.info(ssVO.toString());
+			}
+
+			if ((successCount == deviceListIDs.size() || noDiffCount == deviceListIDs.size()) && errorCount == 0) {
+				retVO.setJobExcuteResult(BaseJobImpl.Result.SUCCESS);
+			} else if (errorCount == deviceListIDs.size()) {
+				retVO.setJobExcuteResult(BaseJobImpl.Result.FAILED);
+			} else {
+				retVO.setJobExcuteResult(BaseJobImpl.Result.PARTIAL_SUCCESS);
+			}
+
+		} catch (Exception e) {
+			log.error(e.toString(), e);
+			masterVO.setMessage(e.toString());
+		}
+
+		String msg = "";
+		String[] args = null;
+		if (totalCount == 1) {
+			if (successCount == 1) {
+				msg = "備份成功";
+
+			} else if (errorCount == 1) {
+				msg = "備份失敗";
+
+			} else if (noDiffCount == 1) {
+				msg = "版本無差異";
+			}
+
+		} else {
+			msg = "選定備份 {0} 筆設備: 備份成功 {1} 筆；失敗 {2} 筆；版本無差異 {3} 筆";
+			args = new String[] {
+					String.valueOf(totalCount),
+					String.valueOf(successCount),
+					String.valueOf(errorCount),
+					String.valueOf(noDiffCount)
+			};
+		}
+
+		masterVO.setEndTime(new Date());
+		masterVO.setResult(CommonUtils.converMsg(msg, args));
+
+		try {
+			provisionService.insertProvisionLog(masterVO);
+		} catch (ServiceLayerException e) {
+			log.error(e.toString(), e);
+		}
+
+		retVO.setRetMsg(CommonUtils.converMsg(msg, args));
+		retVO.setJobExcuteRemark(retVO.getRetMsg());
+
+		return retVO;
+	}
+	
+	@Override
+	public VersionServiceVO backupConfig(String configType, List<String> deviceListIDs, boolean jobTrigger, String triggerName, String deviceUsername, String deviceIp) {
+		ProvisionServiceVO masterVO = new ProvisionServiceVO();
+		VersionServiceVO retVO = new VersionServiceVO();
+		final int totalCount = deviceListIDs.size();
+		retVO.setJobExcuteResultRecords(Integer.toString(totalCount));
+
+		int successCount = 0;
+		int errorCount = 0;
+		int noDiffCount = 0;
+
+		try {
+			masterVO.setLogMasterId(UUID.randomUUID().toString());
+			masterVO.setBeginTime(new Date());
+			// masterVO的userName和userIp會紀錄進provision_log_detail
+			masterVO.setUserName(jobTrigger ? Env.USER_NAME_JOB : (StringUtils.isBlank(deviceUsername)?SecurityUtil.getSecurityUser().getUsername():deviceUsername));
+			masterVO.setUserIp(deviceIp);
+			masterVO.setTriggerName(triggerName); //provision_log_detail.create_by參考此項
 			StepServiceVO ssVO;
 			for (String deviceListId : deviceListIDs) {
 				ssVO = stepService.doBackupStep(deviceListId, jobTrigger);
